ff1720e9834a80e103d96bfa618cb613c30607d8
Muhammad Firdaus Bin Mohd Fauzi <ffdausfauzi8@gmail.com>
2024-04-16 10:55:56 +0800

    Update MainActivity, Create ViewModel & Adapter

---
+++ app/src/main/java/com/example/notesapp/viewmodel/NoteViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.notesapp.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.room.Query
+import com.example.notesapp.model.Note
+import com.example.notesapp.repository.NoteRepository
+import kotlinx.coroutines.launch
+
+class NoteViewModel(app: Application, private val noteRepository: NoteRepository): AndroidViewModel(app) {
+    //as a bridge between view & model
+
+    fun addNote(note: Note) =
+        viewModelScope.launch {
+            noteRepository.insertNote(note)
+        }
+    fun updateNote(note: Note) =
+        viewModelScope.launch {
+            noteRepository.updateNote(note)
+        }
+    fun deleteNote(note: Note) =
+        viewModelScope.launch {
+            noteRepository.deleteNote(note)
+        }
+    fun getALlNotes() =
+        noteRepository.getAllNotes()
+
+    fun searchNote(query: String?) =
+        noteRepository.searchNote(query)
+}
